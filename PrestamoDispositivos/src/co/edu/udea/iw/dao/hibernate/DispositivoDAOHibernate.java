afb623b090e08db3fd6e9c2295609ab79bfba16b
pipe <pipe@pipe-Inspiron-5521>
2016-05-07 22:21:37 -0500

    Prueba de dispositivo no funciona, prueba de usuario sin implementar, prueba de persona sí funciona

--- PrestamoDispositivos/src/co/edu/udea/iw/dao/hibernate/DispositivoDAOHibernate.java
+++ PrestamoDispositivos/src/co/edu/udea/iw/dao/hibernate/DispositivoDAOHibernate.java
@@ -1,5 +1,76 @@
 package co.edu.udea.iw.dao.hibernate;
 
-public class DispositivoDAOHibernate {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import co.edu.udea.iw.dao.DispositivoDAO;
+import co.edu.udea.iw.dto.Dispositivo;
+import co.edu.udea.iw.exception.IWDaoException;
+
+public class DispositivoDAOHibernate extends HibernateDaoSupport implements DispositivoDAO{
+
+	@Override
+	public Dispositivo insertar(Dispositivo dispositivo) throws IWDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dispositivo modificar(Dispositivo dispositivo) throws IWDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Dispositivo> obtener() throws IWDaoException {
+
+		List<Dispositivo> dispositivos = new ArrayList<Dispositivo>();
+		Session session = null;
+		
+		try{
+			
+			session = getHibernateTemplate().getSessionFactory().getCurrentSession();
+			Criteria criteria = session.createCriteria(Dispositivo.class);
+			dispositivos = criteria.list();
+			
+		}catch(HibernateException e){
+			throw new IWDaoException(e);
+		}
+		
+		return dispositivos;
+	}
+
+	@Override
+	public Dispositivo borrar(Dispositivo dispositivo) throws IWDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dispositivo obtenerId(Integer idDispositivo) throws IWDaoException {
+		Session session = null;
+		Dispositivo dispositivo = null;
+		
+		try{
+			session = getHibernateTemplate().getSessionFactory().getCurrentSession();			
+			dispositivo = (Dispositivo)session.get(Dispositivo.class, idDispositivo);
+		}catch(HibernateException e){
+			throw new IWDaoException(e);
+		}
+		
+		return dispositivo;
+	}
+	
+
+	@Override
+	public Dispositivo obtenerNombre(String nombre) throws IWDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
---
+++ PrestamoDispositivos/test/co/edu/udea/iw/dao/hibernate/DispositivoDAOHibernateTest.java
@@ -0,0 +1,39 @@
+package co.edu.udea.iw.dao.hibernate;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.udea.iw.dao.DispositivoDAO;
+import co.edu.udea.iw.dto.Dispositivo;
+import co.edu.udea.iw.exception.IWDaoException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations = "classpath:configuracion.xml")
+public class DispositivoDAOHibernateTest {
+	@Autowired
+	DispositivoDAO dispositivoDao;
+
+	@Test
+	private void testObtener() {
+		List<Dispositivo> dispositivos;
+		try {
+			dispositivos = dispositivoDao.obtener();
+			for (Dispositivo dispositivo : dispositivos) {
+				System.out.println("Nombre: " + dispositivo.getNombre());
+			}
+			assertTrue(true);
+		} catch (IWDaoException e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
